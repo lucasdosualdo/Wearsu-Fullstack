55e6ac6de581a507e404f2e4e2e215944b7b5e3c
Your Name <lucasdosualdo@gmail.com>
2023-05-27 16:46:21 -0300

    feat: config backend envirolment

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import app, { init } from "./app";
+
+const port = +process.env.PORT || 5000;
+
+init()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}.`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error occurred while initializing the server:", error);
+  });
